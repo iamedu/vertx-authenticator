11b9ae1cbb7b4cdb6c9dbb7a2fc553ff528638a2
Eduardo Díaz <iamedu@gmail.com>
2014-01-20 10:00:49 -0600

    Adding deps

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/iamedu/vertx-authenticator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [com.lambdaworks/scrypt "1.4.0"]]
   :plugins [[lein-vertx "0.2.0-SNAPSHOT"]]
   :vertx {:main iamedu.vertx-authenticator.core/init
           :author "Eduardo Díaz"
